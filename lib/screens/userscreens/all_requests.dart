52ba19dc85ceea0a036a5066e1c3a4f725988d0b
Gabula Stephen <johnstephengabula@gmail.com>
2023-02-07 08:15:26 +0300

    Update all_requests.dart

--- lib/screens/userscreens/all_requests.dart
+++ lib/screens/userscreens/all_requests.dart
@@ -15,15 +15,28 @@ class _RequestsState extends State<Requests> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Blood Requests"),
+      ),
       body: StreamBuilder<QuerySnapshot>(
         stream: requestsStream,
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) {
-            return const Text('Something went wrong');
+            return Center(
+              child: Text("Something went wrong"),
+            );
           }
 
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: Text("Loading"));
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Text("No Blood Requests Available"),
+            );
           }
 
           return ListView(
@@ -34,25 +47,23 @@ class _RequestsState extends State<Requests> {
                 padding: const EdgeInsets.only(top: 10, bottom: 10),
                 child: ListTile(
                   shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(15),
-                          topRight: Radius.circular(15),
-                          bottomRight: Radius.circular(15),
-                          bottomLeft: Radius.circular(15))),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
                   tileColor: Colors.green[200],
                   title: RichText(
                       text: TextSpan(
                           text: "Location: ${data['location']} \n",
                           style: const TextStyle(color: Colors.black),
                           children: [
-                        TextSpan(text: "BloodAmount: ${data['blood_amount']}")
+                        TextSpan(
+                            text: "Blood Amount: ${data['blood_amount']} mL")
                       ])),
                   subtitle: RichText(
                       text: TextSpan(
-                          text: "BloodGroup: ${data['blood_group']} \n",
+                          text: "Blood Group: ${data['blood_group']} \n",
                           style: const TextStyle(color: Colors.black),
                           children: [
-                        TextSpan(text: "Date of Need: ${data['Date_of_Need']}")
+                        TextSpan(
+                            text: "Date of Need: ${data['Date_of_Need']}")
                       ])),
                   trailing: Chip(label: Text(data['status'])),
                 ),
