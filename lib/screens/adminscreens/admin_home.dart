98ddb286c08fa5c6c9cfdf6323f62a0fb5128c77
Gabula Stephen <johnstephengabula@gmail.com>
2023-02-08 13:20:39 +0300

    Update admin_home.dart

--- lib/screens/adminscreens/admin_home.dart
+++ lib/screens/adminscreens/admin_home.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:musayi/screens/adminscreens/approving_page.dart';
 
+// Class for creating the admin home screen 
 class AdminHome extends StatefulWidget {
   const AdminHome({Key? key}) : super(key: key);
 
@@ -9,9 +10,12 @@ class AdminHome extends StatefulWidget {
   State<AdminHome> createState() => _AdminHomeState();
 }
 
+// Stateful class for the admin home screen
 class _AdminHomeState extends State<AdminHome> {
+  // Stream of data from the blood requests collection in Firestore
   final Stream<QuerySnapshot> requestsStream =
       FirebaseFirestore.instance.collection('bloodRequest').snapshots();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,28 +23,34 @@ class _AdminHomeState extends State<AdminHome> {
       body: StreamBuilder<QuerySnapshot>(
         stream: requestsStream,
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          // Error handling
           if (snapshot.hasError) {
             return const Text('Something went wrong');
           }
 
+          // Loading indication
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: Text("Loading"));
           }
 
           return ListView(
             children: snapshot.data!.docs.map((DocumentSnapshot document) {
+              // Map of the document data
               Map<String, dynamic> data =
                   document.data()! as Map<String, dynamic>;
               return Padding(
                 padding: const EdgeInsets.only(top: 10, bottom: 10),
                 child: ListTile(
+                  // Rounded rectangle border for the list tile
                   shape: const RoundedRectangleBorder(
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(15),
                           topRight: Radius.circular(15),
                           bottomRight: Radius.circular(15),
                           bottomLeft: Radius.circular(15))),
+                  // Background color of the list tile
                   tileColor: Colors.green[200],
+                  // Title text of the list tile
                   title: RichText(
                       text: TextSpan(
                           text: "Location: ${data['location']} \n",
@@ -48,6 +58,7 @@ class _AdminHomeState extends State<AdminHome> {
                           children: [
                         TextSpan(text: "BloodAmount: ${data['blood_amount']}")
                       ])),
+                  // Subtitle text of the list tile
                   subtitle: RichText(
                       text: TextSpan(
                           text: "BloodGroup: ${data['blood_group']} \n",
@@ -55,7 +66,9 @@ class _AdminHomeState extends State<AdminHome> {
                           children: [
                         TextSpan(text: "Date of Need: ${data['Date_of_Need']}")
                       ])),
+                  // Trailing chip with the status of the request
                   trailing: Chip(label: Text(data['status'])),
+                  // On tap action
                   onTap: () {
                     print(
                         "${FirebaseFirestore.instance.collection('bloodRequest').doc(document.id)}");
